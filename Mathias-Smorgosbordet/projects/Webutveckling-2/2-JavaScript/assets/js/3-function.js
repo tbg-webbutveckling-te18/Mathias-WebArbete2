9fd7b91be4a9b13acc2f8557fcf7a012697a32eb
Mathias Karlsson <kristina.e.karlsson@spray.se>
2020-10-14 11:31:05 +0200

    Jobs done

---
+++ Mathias-Smorgosbordet/projects/Webutveckling-2/2-JavaScript/assets/js/3-function.js
@@ -0,0 +1,67 @@
+
+
+document.writeln('Blinka lila stjärna där <br>')
+document.writeln('Hur jag undrar var du är <br>')
+document.writeln('Fjärran lockar du min syn <br>')
+document.writeln('Lik en diamant i skyn <br>')
+
+document.writeln('<br>')
+
+document.writeln('Blinka lila stjärna där <br>')
+document.writeln('Hur jag undrar var du är <br>')
+document.writeln('Fjärran lockar du min syn <br>')
+document.writeln('Lik en diamant i skyn <br>')
+
+document.writeln('-----------------------------------------------------------------------------------<br>')
+
+function twinkle() {
+    document.writeln('Blinka lila stjärna där <br>')
+    document.writeln('Hur jag undrar var du är <br>')
+    document.writeln('Fjärran lockar du min syn <br>')
+    document.writeln('Lik en diamant i skyn <br>')
+
+    document.writeln('<br>')
+}
+
+twinkle();
+twinkle();
+
+document.writeln('-----------------------------------------------------------------------------------<br>')
+
+function square(num) {
+    document.writeln(num * num)
+}
+
+square(10)
+
+
+
+
+
+document.writeln('<br>-----------------------------------------------------------------------------------<br>')
+
+
+function area(width, height) {
+    return width * height;
+}
+
+document.writeln('<br> The area is: ' + area(5, 10))
+
+var result = area(6, 9);
+
+document.writeln('<br>' + result)
+
+function quadropelme(x) {
+    return 4 * x;
+}
+
+var myNumber = quadroplme(4);
+alert(myNumber);
+
+document.writeln('<br>-----------------------------------------------------------------------------------<br>')
+
+
+
+
+
+
