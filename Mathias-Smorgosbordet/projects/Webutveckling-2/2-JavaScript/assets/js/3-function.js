444ffd832bc49877084abe832a440af2d70d84b4
Mathias Karlsson <kristina.e.karlsson@spray.se>
2020-10-16 15:07:58 +0200

    Doot done

--- Mathias-Smorgosbordet/projects/Webutveckling-2/2-JavaScript/assets/js/3-function.js
+++ Mathias-Smorgosbordet/projects/Webutveckling-2/2-JavaScript/assets/js/3-function.js
@@ -1,6 +1,6 @@
 
 
-document.writeln('Blinka lila stjärna där <br>')
+document.writeln('<br> Blinka lila stjärna där <br>')
 document.writeln('Hur jag undrar var du är <br>')
 document.writeln('Fjärran lockar du min syn <br>')
 document.writeln('Lik en diamant i skyn <br>')
@@ -55,10 +55,54 @@ function quadropelme(x) {
     return 4 * x;
 }
 
-var myNumber = quadroplme(4);
-alert(myNumber);
+var myNumber = quadropelme(4)
+
+/* Select an element */
+
+var btnNumber = document.getElementById("myNumber");
+
+// Manipulate element
+btnNumber.innerHTML = "My favorite number is " + myNumber
+
+// Function att göra första bokstaven stor
+function capitalize(str) {
+    return str.charAt('').toUpperCase() + str.slice(1);
+}
+
+var city = 'stockholm';
+var capital = capitalize(city);
+document.writeln('<br> Captial of Sweden is: ' + capital)
 
 document.writeln('<br>-----------------------------------------------------------------------------------<br>')
+
+var numberStart = 0;
+var numberEven = numberStart++;
+
+function isEven(numberEven) {
+    if (numberEven % 2 == 0) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+document.writeln('Even numbers are: ' + isEven(2))
+
+document.writeln('<br>-----------------------------------------------------------------------------------<br>')
+
+
+
+function kebabToSnake(str) {
+    var snakeCasedStr = str.replace(/-/, '_')
+    return snakeCasedStr;
+}
+
+document.writeln(kebabToSnake('Hej-jag-heter-Mathias'))
+
+
+
+
 
 
 
